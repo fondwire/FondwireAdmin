034f9797ea55d974c8b31b97e24233b14ac59cff
Aman <aman_A00@mail.ru>
2021-01-15 01:25:32 +0600

    support request mail sender function

--- functions/index.js
+++ functions/index.js
@@ -23,7 +23,9 @@ exports.sendEmailNotification = functions.firestore.document('/mail/{mailId}').o
                     <div> <b>Fullname: </b> <span>${data.fullname}</span> </div>
                     <div> <b>Email: </b> <span>${data.email}</span> </div>
                     <div> <b>Company name: </b> <span>${data.companyName}</span> </div>
-                    <div> <b>Position: </b> <span>${data.position}</span> </div>
+                    <div> <b>Phone: </b> <span>${data.phone}</span> </div>
+                    <div> <b>Regarding: </b> <span>${data.regarding}</span> </div>
+                    <div> <b>Message: </b> <span>${data.message}</span> </div>
                 </div>
             ` // email content in HTML
         };
@@ -31,6 +33,30 @@ exports.sendEmailNotification = functions.firestore.document('/mail/{mailId}').o
         transporter.sendMail(mailOptions, (err) => {
             if(err){
                 console.log(err)
+                return err.toString()
+            }
+            console.log('Email sent successfully')
+            return 'Email sent successfully'
+        });
+        // return authData.sendMail(mailOptions).then((res)=> console.log('successfully sent that mail', res.toString()) ).catch((error) => console.log('error )) : ',error))
+    }
+)
+
+exports.sendEmailSupport = functions.firestore.document('/support/{supportId}').onCreate(
+    async (snapshot) => {
+        const data = await snapshot.data()
+        const mailOptions = {
+            from: '"Fundwire support request" <info.fundwire@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
+            to: "fintechfondwireapp@gmail.com",
+            subject: `<div>${data.name}</div>`, // email subject
+            html: `
+                <div>
+                    ${JSON.stringify(data)}
+                </div>
+            ` // email content in HTML
+        };
+        transporter.sendMail(mailOptions, (err) => {
+            if(err){
                 return err.toString()
             }
             console.log('Email sent successfully')
